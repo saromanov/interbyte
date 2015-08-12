6bf024c453de4961375a140284202542cc070c95
Sergey Romanov <xxsmotur@gmail.com>
2015-08-12 23:27:30 +0500

    Update

--- interbyte.py
+++ interbyte.py
@@ -5,6 +5,7 @@ import inspect
 class VM:
     def __init__(self):
         self.stack = []
+        self.names = {}
 
     def fit(self, code, globals=None, locals=None):
         if code == '':
@@ -28,3 +29,9 @@ class VM:
     def _parse(self, code):
         ''' After compiled, parse result'''
         return dis.opname[code]
+
+    def _process_opcode(self, opcode, item):
+        if opcode == 'LOAD_CONST':
+            self.stack.extend(item)
+        if opcode == 'STORE_NAME':
+            self.names[item] = self.stack.pop()
